d04423004815e5063ab0991d05cbae8f44fde9d3
x7510834-crypto <x7510834@gmail.com>
2025-10-02 12:33:49 +0530

    Create FirstController.java

---
+++ com/example/controller/FirstController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FirstController.class);
+
+    @GetMapping("/first")
+    public String first() {
+        logger.info("FirstController accessed");
+        try {
+            int result = 10 / 0; // will throw ArithmeticException
+        } catch (Exception e) {
+            logger.error("Exception in FirstController", e);
+            throw e;
+        }
+        return "First Controller";
+    }
+}
