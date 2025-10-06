b2c48b149a56a44a0895be37d80cc646c01e2c3d
Dhaval <x7510834@gmail.com>
2025-10-06 16:57:44 +0530

    manual commit for checking - 2

--- com/example/controller/SecondController.java
+++ com/example/controller/SecondController.java
@@ -23,9 +23,7 @@ public class SecondController {
                 if(b != null) {
                     if(c !=null) {
                         if(d != null) {
-                              if (e != null) {
                                 e.length();
-                              }
                         }
                     }
                 }
